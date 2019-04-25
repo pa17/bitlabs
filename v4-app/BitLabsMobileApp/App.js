1e1c36da1a6f5b3d49c446cf39e7fb05d9f7d3ef
Alex Gourlay <alexdgourlay@gmail.com>
2019-04-25 14:17:53 +0100

    Update

--- v4-app/BitLabsMobileApp/App.js
+++ v4-app/BitLabsMobileApp/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-import { DeviceMotionSensor } from './src/components/Main'
-import { WebSocketContext, WebSocketManager } from './src/contexts/WebSocketContext'
+import { Main } from './src/components/Main'
+import { WebSocketContext, webSocketManager } from './src/contexts/WebSocketContext'
 import { ControlModeContext, EffectSelection } from './src/contexts/ControlModeContext';
 
 export default class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.WebSocketManager = new WebSocketManager('146.169.153.168');
-  }
 
   render() {
     return (
       <ControlModeContext.Provider value={{ mode: EffectSelection }}>
-        <WebSocketContext.Provider value={{ wsm: this.WebSocketManager }}>
-          <DeviceMotionSensor />
+        <WebSocketContext.Provider value={{ wsm: webSocketManager }}>
+          <Main />
         </WebSocketContext.Provider>
       </ControlModeContext.Provider>
     );
