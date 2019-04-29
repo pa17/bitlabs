c3c7f607952badf5b2de34964c0e59635474286c
Alex Gourlay <alexdgourlay@gmail.com>
2019-04-29 12:28:25 +0100

    Restored Changes

--- v4-app/BitLabsMobileApp/src/contexts/ControlModeContext.js
+++ v4-app/BitLabsMobileApp/src/contexts/ControlModeContext.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 
 // Context to hold all socket communication.
 export const ControlModeContext = React.createContext();
@@ -6,7 +7,9 @@ export const ControlModeContext = React.createContext();
 class _EffectSelection {
 
     constructor() {
+        this.id = "EffectSelection";
         this.buttonsActive = [true, false, false, false, false, false, false, false];
+        this.toggleActive = false;
     }
   
     handleButtonPress(index)  {
@@ -14,30 +17,102 @@ class _EffectSelection {
         this.buttonsActive[index] = true;
     }
 
-    // GET METHODS
     getButtonsActive(index) {
-        return this.buttonsActive[index];
+        if (this.buttonsActive[index])
+        { 
+            return styles.smallSelected.fill;
+        }
+        else 
+        {
+            return styles.smallUnselected.fill 
+        } 
+    }
+
+    getToggleActive() {
+        if (this.toggleActive)
+        { 
+            return styles.toggleSelected.fill;
+        }
+        else 
+        {
+            return styles.toggleUnselected.fill 
+        } 
+    }
+
+    updateEffectAmount(effectAmount) {
+        return styles.outsideCircle.fill;
     }
 }
 
 class _EffectControl {
 
     constructor() {
-        this.buttonsActive = [true, false, false, false, false, false, false, false];
+        this.id = "EffectControl",
+        this.buttonsActive = [false, false, false, false, false, false, false, false];
+        this.toggleActive = true;
+        this.effectAmount = 0;
     }
   
-    handleButtonPress(index)  {
-        console.log(`Effect ${index} selected`);
-        this.buttonsActive = [false, false, false, false, false, false, false, false, false];
-        this.buttonsActive[index] = true;
-    }
+    handleButtonPress(index)  {} // UNUSED IN THIS MODE
 
     // GET METHODS
     getButtonsActive(index) {
-        return this.buttonsActive[index];
+        if (this.effectAmount >= (index + 1 ) * (1 / 12) - (1 / 24)) {
+            return styles.smallEffectActive.fill;
+        }
+        else {
+            return styles.smallEffectInactive.fill;
+        }
+    }
+
+    getToggleActive(effectAmount) {
+        if (this.toggleActive)
+        { 
+            return styles.toggleSelected.fill;
+        }
+        else 
+        {
+            return styles.toggleUnselected.fill 
+        } 
+    }
+
+    updateEffectAmount(effectAmount) {
+        this.effectAmount = effectAmount;
+
+        return styles.outsideCircle.fill;
     }
 }
 
 export let EffectSelection = new _EffectSelection();
+export let EffectControl = new _EffectControl();
 
+const styles = StyleSheet.create({
 
+    outsideCircle : {
+        fill : 'slategray'
+    },
+
+    smallSelected : {
+      fill : 'pink',
+    },
+  
+    smallUnselected : {
+      fill : 'lightsteelblue'
+    },
+
+    smallEffectActive: {
+        fill: 'white'
+    },
+
+    smallEffectInactive: {
+        fill: 'grey'
+    },
+
+    toggleSelected : {
+        fill : 'salmon',
+      },
+    
+    toggleUnselected : {
+        fill : 'white'
+    },
+});
