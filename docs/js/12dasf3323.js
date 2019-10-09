a91febc49b853ef9c135d340f8052e40c6ee5c5e
Alex Gourlay <alexdgourlay@gmail.com>
2019-10-09 14:59:25 +0100

    Removed Password Protection

--- docs/js/12dasf3323.js
+++ docs/js/12dasf3323.js
@@ -1,19 +1,24 @@
+// function passWord() {
+//     var testV = 1;
+//     var pass1 = prompt('Please Enter Your Password', ' ');
+//     while (testV < 3) {
+//         if (!pass1)
+//             history.go(-1);
+//         if (pass1.toLowerCase() == "ero2019") {
+//             alert('Password correct!');
+//             window.open('private.html');
+//             break;
+//         }
+//         testV += 1;
+//         var pass1 =
+//             prompt('Access Denied - Password Incorrect, Please Try Again.', 'Password');
+//     }
+//     if (pass1.toLowerCase() != "password" & testV == 3)
+//         history.go(-1);
+//     return " ";
+// } 
+
+// Disable password protection
 function passWord() {
-    var testV = 1;
-    var pass1 = prompt('Please Enter Your Password', ' ');
-    while (testV < 3) {
-        if (!pass1)
-            history.go(-1);
-        if (pass1.toLowerCase() == "ero2019") {
-            alert('Password correct!');
-            window.open('private.html');
-            break;
-        }
-        testV += 1;
-        var pass1 =
-            prompt('Access Denied - Password Incorrect, Please Try Again.', 'Password');
-    }
-    if (pass1.toLowerCase() != "password" & testV == 3)
-        history.go(-1);
-    return " ";
-} 
+    window.open('private.html');
+}
